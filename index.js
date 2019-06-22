4c58d83033fc013cdfc90b7313e0225c09ded308
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 20:49:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,12 @@
   // trips() - returns all of the trips that a passenger has taken
   // drivers() - returns all of the drivers that has taken a
   // passenger on a trip
+
 // Trip class:
-//
-// A trip belongs to a driver and belongs to a passenger.
-// new Trip() - initialized with an instance of driver and an instance of passenger; returns a JavaScript that object has attributes id, driverId, and passengerId
-// driver() - returns the driver associated with the trip
-// passenger() - returns the passenger associated with the trip
+  // A trip belongs to a driver and belongs to a passenger.
+  // new Trip() - initialized with an instance of driver and an
+  // instance of passenger; returns a JavaScript that object has
+  // attributes id, driverId, and passengerId
+
+  // driver() - returns the driver associated with the trip
+  // passenger() - returns the passenger associated with the trip
