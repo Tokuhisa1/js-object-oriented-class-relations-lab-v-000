970369369e7a15ec8fb1d3bd90a4269e84f70262
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 22:37:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ class Passenger {
   // drivers() - returns all of the drivers that has taken a
   // passenger on a trip
   drivers() {
-    return this.trips().map(
+    return trips().map(
       function(trip) {
         return trip.driver();
       }
