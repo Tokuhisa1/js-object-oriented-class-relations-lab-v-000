0124ab8b0745ce1b5d5625b02e1bedf61a1f7403
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 22:40:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,90 @@
+let store = { drivers: [], passengers: [], trips: [] };
+let driverId = 0;
+let passengerId = 0;
+let tripId = 0;
+// Driver class:
+class Driver {
+  // A driver has many trips, and has many passengers through
+  // trips.
+  constructor(name) {
+    // new Driver() - initialized with a name; returns a
+    // JavaScript object that has attributes of id, and name
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
+  }
+  // trips() - returns all of the trips that a driver has taken
+  trips() {
+      return store.trips.filter(
+          function(trip) {
+              return trip.driverId === this.id;
+          }.bind(this)
+      );
+  }
+  // passengers() - returns all of the passengers that a driver
+  // has taken on a trip
+  passengers() {
+    return this.trips().map(
+      function(trip) { return trip.passenger(); }
+    );
+  }
+}
+// Passenger class:
+class Passenger {
+  // A passenger has many trips, and has many drivers through
+  // trips.
+  constructor(name) {
+    // new Passenger() - initialized with a name; returns a
+    // JavaScript object that has attributes of id, and name
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
+  }
+  // trips() - returns all of the trips that a passenger has taken
+  trips() {
+      return store.trips.filter(
+          function(trip) {
+              return trip.passengerId === this.id;
+          }.bind(this)
+      );
+  }
+  // drivers() - returns all of the drivers that has taken a
+  // passenger on a trip
+  drivers() {
+    return this.trips().map(
+      function(trip) { return trip.driver(); }
+    );
+  }
+}
+// Trip class:
+class Trip {
+  // A trip belongs to a driver and belongs to a passenger.
+  constructor(driver, passenger) {
+    // new Trip() - initialized with an instance of driver and an
+    // instance of passenger; returns a JavaScript that object has
+    // attributes id, driverId, and passengerId
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+
+    store.trips.push(this);
+  }
+  // driver() - returns the driver associated with the trip
+  driver() {
+      return store.drivers.find(
+          function(driver) {
+              return driver.id === this.driverId;
+          }.bind(this)
+      );
+  }
+  // passenger() - returns the passenger associated with the trip
+  passenger() {
+      return store.passengers.find(
+          function(passenger) {
+              return passenger.id === this.passengerId;
+          }.bind(this)
+      );
+  }
+}
