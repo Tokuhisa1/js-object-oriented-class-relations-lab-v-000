6f49dc56b284d3b8fae7bba6acb7fe387c1a3ccb
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-06-22 20:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Driver class:
-//
-// A driver has many trips, and has many passengers through trips.
-// new Driver() - initialized with a name; returns a JavaScript object that has attributes of id, and name
-// trips() - returns all of the trips that a driver has taken
-// passengers() - returns all of the passengers that a driver has taken on a trip
+  // A driver has many trips, and has many passengers through
+  // trips.
+  
+  // new Driver() - initialized with a name; returns a JavaScript
+  // object that has attributes of id, and name
+  
+  // trips() - returns all of the trips that a driver has taken
+  // passengers() - returns all of the passengers that a driver
+  // has taken on a trip
+  
 // Passenger class:
 //
 // A passenger has many trips, and has many drivers through trips.
